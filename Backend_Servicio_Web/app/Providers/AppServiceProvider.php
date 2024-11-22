5a99c12b3581dad3c35983f2d98a7cfbc6bb8cea
JuanLacouture <berriro28@gmail.com>
2024-11-21 23:02:22 -0500

    Revert "chi"
    This reverts commit fcc088a9a2ec3034dafa60f35a35e09a4bcebe15.

--- Backend_Servicio_Web/app/Providers/AppServiceProvider.php
+++ Backend_Servicio_Web/app/Providers/AppServiceProvider.php
@@ -22,9 +22,7 @@ class AppServiceProvider extends ServiceProvider
      * @return void
      */
     public function boot()
-{
-    if (config('app.env') === 'production') {
-        URL::forceScheme('https');
+    {
+        //
     }
-}
 }
